ab6213db2badf07c4e5ecb0a864e61425d198300
Max Frederiksen <max@colesmedia.se>
2020-01-08 13:14:14 +0100

    hotfix  error

--- source/php/Helper/AutoCreate.php
+++ source/php/Helper/AutoCreate.php
@@ -4,23 +4,19 @@ namespace adApiWpIntegration\Helper;
 
 class AutoCreate
 {
-
     private $defaultRole;
 
-    public function __construct() {
-        $this->defaultRole = defined('AD_AUTOCREATE_ROLE') && get_role(AD_AUTOCREATE_ROLE) ? AD_AUTOCREATE_ROLE : "subscriber";
-    }
-
-    public static function autoCreateUser($userName, $passWord)
+    public static function autoCreateUser($username, $password)
     {
+        $defaultRole = defined('AD_AUTOCREATE_ROLE') && get_role(AD_AUTOCREATE_ROLE) ? AD_AUTOCREATE_ROLE : "subscriber";
         try {
             $insertUser = wp_insert_user(
-                array(
-                    'user_login' => $userName,
-                    'user_pass' => $passWord,
-                    'role' =>  $this->defaultRole
-                )
-            );
+              array(
+                  'user_login' => $username,
+                  'user_pass' => $password,
+                  'role' =>  $defaultRole
+              )
+          );
         } catch (\Exception $e) {
             error_log("Error: Could not create a new user using bulk data (ad-api-integration).");
         }
