ca7b034261aad14cb6e9b4795a7d9764d134da96
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-01-09 14:16:34 +0100

    Add to blog, if multisite

--- source/php/Helper/AutoCreate.php
+++ source/php/Helper/AutoCreate.php
@@ -11,12 +11,17 @@ class AutoCreate
         $defaultRole = defined('AD_AUTOCREATE_ROLE') && get_role(AD_AUTOCREATE_ROLE) ? AD_AUTOCREATE_ROLE : "subscriber";
         try {
             $insertUser = wp_insert_user(
-              array(
-                  'user_login' => $username,
-                  'user_pass' => $password,
-                  'role' =>  $defaultRole
-              )
-          );
+                array(
+                    'user_login' => $username,
+                    'user_pass' => $password,
+                    'role' =>  $defaultRole
+                )
+            );
+
+          if(is_numeric($insertUser) && is_multisite()) {
+              add_user_to_blog(get_current_blog_id(), $insertUser, $defaultRole); 
+          }
+
         } catch (\Exception $e) {
             error_log("Error: Could not create a new user using bulk data (ad-api-integration).");
         }
